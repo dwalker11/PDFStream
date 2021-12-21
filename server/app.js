f39de13b84994c451624866e64de327bdf422be2
Devon Walker <dwalker11@users.noreply.github.com>
2021-12-21 17:19:24 -0500

    Minor update 2

--- server/app.js
+++ server/app.js
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use(express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
